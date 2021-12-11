cc4287dc3af693c0982d2295392119af0941660d
İsmail HASIR <ismailhasir@gmail.com>
2021-12-11 07:32:35 +0300

    re write

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,59 @@
-const cityForm = document.querySelector(".from");
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
-const updateCity = async () => {};
+const updateUI = (data) => {
+  // destructure properties
+  const { cityDets, weather } = data;
 
-cityForm.addEventListener("Submit", (e) => {
-  //preven default action
+  // update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
+
+  // update the night/day & icon images
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+  
+  let timeSrc = null;
+  if(weather.IsDayTime){
+    timeSrc = 'img/day.svg';
+  } else {
+    timeSrc = 'img/night.svg';
+  }
+  time.setAttribute('src', timeSrc);
+
+  // remove the d-none class if present
+  if(card.classList.contains('d-none')){
+    card.classList.remove('d-none');
+  }
+};
+
+const updateCity = async (city) => {
+
+  const cityDets = await getCity(city);
+  const weather = await getWeather(cityDets.Key);
+  return { cityDets, weather };
+
+};
+
+cityForm.addEventListener('submit', e => {
+  // prevent default action
   e.preventDefault();
-
+  
+  // get city value
   const city = cityForm.city.value.trim();
   cityForm.reset();
 
-  //update UI with the new city
+  // update the ui with new city
+  updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
 });
